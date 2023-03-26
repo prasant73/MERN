a26e0ca60e8a0601e06798250dac7ae80de86184
Prasant Mishra <mishraprasant73@gmail.com>
2023-03-26 13:29:50 +0530

    functional and class components, jsx, props, states, component lifecycles and methods, conditional rendering, list, keys, basics of form handling

--- 2023_03_25_react/contact_manager/src/components/App.js
+++ 2023_03_25_react/contact_manager/src/components/App.js
@@ -1,33 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Swaroop",
-      email: "swaroop@sai.kumar.com"
-    },
-    {
-      id: "2",
-      name: "ankit",
-      email: "ankit@kumar.com"
-    },
-    {
-      id: "3",
-      name: "Subhash",
-      email: "subhash@kd.com"
-    }
-  ];
+  // const contacts = [
+  //   {
+  //     id: "1",
+  //     name: "Swaroop",
+  //     email: "swaroop@sai.kumar.com"
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "ankit",
+  //     email: "ankit@kumar.com"
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Subhash",
+  //     email: "subhash@kd.com"
+  //   }
+  // ];
+  const [contacts, setContacts] = useState([]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts,contact]);
+  }
   return (
     // <h1>Hello World!</h1>
 
     <div className="ui container">
       <Header></Header>
-      <AddContact></AddContact>
+      <AddContact addContactHandler={addContactHandler}></AddContact>
       <ContactList contacts={contacts}></ContactList>
     </div>
     
