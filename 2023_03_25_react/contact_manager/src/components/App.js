c5ed033e9416a95a73f9532ad49edaeb62918db6
Prasant Mishra <mishraprasant73@gmail.com>
2023-04-02 12:01:13 +0530

    localstorage, useEffect, delete items etc

--- 2023_03_25_react/contact_manager/src/components/App.js
+++ 2023_03_25_react/contact_manager/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from 'uuid';
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -22,18 +23,60 @@ function App() {
   //     email: "subhash@kd.com"
   //   }
   // ];
+
+  const LOCAL_STORAGE_KEY = "contacts";
+
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts,contact]);
+    setContacts([...contacts, { id: uuid(), ...contact}]);
   }
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    })
+    setContacts(newContactList);
+  }
+
+  useEffect(() => {
+    let retrieveContacts = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  // useEffect(() => {
+  //   window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  // }, [contacts]);
+
+  useEffect(() =>{ if(contacts.length > 0)
+    {localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }}, [contacts]);
+
+
+// const [contacts, setContacts] = useState(() => {
+//   const retrieveData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+//   return retrieveData ? retrieveData : [];
+// });
+
+// const addContactHandler = (contact) => {
+//   setContacts((prevContacts) => {
+//     return [...prevContacts, contact];
+//   });
+// };
+// useEffect(() => {
+//   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+// }, [contacts]);
+
   return (
     // <h1>Hello World!</h1>
 
     <div className="ui container">
       <Header></Header>
       <AddContact addContactHandler={addContactHandler}></AddContact>
-      <ContactList contacts={contacts}></ContactList>
+      <ContactList 
+        contacts={contacts} 
+        getContactId = {removeContactHandler}
+      ></ContactList>
     </div>
     
     // <ContactList></ContactList>
