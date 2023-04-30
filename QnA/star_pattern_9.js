23f8922a45e906958d6c858fd0fc6aabb0db87f0
Prasant Mishra <mishraprasant73@gmail.com>
2023-04-30 10:37:28 +0530

    added new questions and solutions for reference

---
+++ QnA/star_pattern_9.js
@@ -0,0 +1,38 @@
+/*
+
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+
+*/
+
+
+const readline = require('readline');
+
+const interface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Q1
+
+interface.question('Enter the Character ',(value) => {
+    interface.question('Enter the n times ', (max_num) => {
+        for (let increment = max_num; increment > 0; increment--) {
+            console.log(" ".repeat(increment) + value.repeat(max_num - increment + 1) +
+             value.repeat(max_num - increment));
+        } 
+        for (let increment = 0 ; increment <= max_num; increment++) {
+            console.log(" ".repeat(increment) + value.repeat(max_num - increment + 1) +
+             value.repeat(max_num - increment));
+        }
+        interface.close() 
+    })
+})
